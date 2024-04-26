31b2f1fc2cda2e9560fea3b0ad29db86b4779247
Tashdik <s2110477153@ru.ac.bd>
2024-04-27 02:31:19 +0600

    my added page updated

--- src/pages/myAdd/MyAdd.jsx
+++ src/pages/myAdd/MyAdd.jsx
@@ -24,16 +24,16 @@ const MyAdd = () => {
         fetch(`http://localhost:5000/spots/${id}`, {
             method: "DELETE"
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            if(data.deletedCount){
-                const remaining = spots.filter(spot => spot._id !== id);
-                setSpots(remaining);
-                
-                alert('Deleted');
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount) {
+                    const remaining = spots.filter(spot => spot._id !== id);
+                    setSpots(remaining);
+
+                    alert('Deleted');
+                }
+            })
 
     }
 
@@ -46,10 +46,10 @@ const MyAdd = () => {
                     <thead>
                         <tr>
                             <th></th>
-                            <th>Spot Name</th>
-                            <th>Country</th>
-                            <th>Average Cost</th>
-                            <th>visitors Per Year</th>
+                            <th className="text-lg font-semibold">Spot Name</th>
+                            <th className="text-lg font-semibold">Country</th>
+                            <th className="text-lg font-semibold">Average Cost</th>
+                            <th className="text-lg font-semibold">visitors Per Year</th>
                             <th></th>
                             <th></th>
                         </tr>
@@ -58,16 +58,25 @@ const MyAdd = () => {
                         {/* row 1 */}
                         {
                             spots.map((spot, idx) => <tr key={idx}>
-                            <th>{idx + 1}</th>
-                            <td>{spot.spotName}</td>
-                            <td>{spot.counter}</td>
-                            <td>{spot.avgCost}</td>
-                            <td>{spot.visitorsPerYear}</td>
-                            <td><button><MdOutlineTipsAndUpdates className="text-xl text-rose-600 font-bold" /></button></td>
-                            <td><button onClick={() => handelDelete(spot._id)}><MdOutlineDeleteForever  className="text-xl text-rose-600 font-bold"/></button></td>
-                        </tr>)
+                                <th>{idx + 1}</th>
+                                <td>{spot.spotName}</td>
+                                <td>{spot.counter}</td>
+                                <td>{spot.avgCost}</td>
+                                <td>{spot.visitorsPerYear}</td>
+                                <td><button><MdOutlineTipsAndUpdates className="text-xl text-rose-600 font-bold" /></button></td>
+                                <td><button onClick={() => handelDelete(spot._id)}><MdOutlineDeleteForever className="text-xl text-rose-600 font-bold" /></button></td>
+                            </tr>)
                         }
-                            {/* const {visitorsPerYear, travelTime, spotName, seasonality, photo, avgCost, _id} = spot; */}
+                        {
+                            spots.length === 0 && <tr>
+                                <th></th>
+                                <td className="text-xl font-bold">None</td>
+                                <td className="text-xl font-bold">None</td>
+                                <td className="text-xl font-bold">None</td>
+                                <td className="text-xl font-bold">None</td>
+                            </tr>
+                        }
+                        {/* const {visitorsPerYear, travelTime, spotName, seasonality, photo, avgCost, _id} = spot; */}
                     </tbody>
                 </table>
             </div>
