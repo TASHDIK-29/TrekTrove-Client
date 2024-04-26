4140f8c9ba3c2c6618f8a7a1327a2ef8443b0917
Tashdik <s2110477153@ru.ac.bd>
2024-04-27 02:07:27 +0600

    Successfully delete data from my add page

--- src/pages/myAdd/MyAdd.jsx
+++ src/pages/myAdd/MyAdd.jsx
@@ -1,29 +1,75 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../auth/AuthProvider";
-import SpotCard from "../../components/spotCard/SpotCard";
+import { MdOutlineTipsAndUpdates, MdOutlineDeleteForever } from "react-icons/md";
 
 const MyAdd = () => {
 
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
+    console.log(user);
 
     const [spots, setSpots] = useState([]);
     console.log(spots);
-    useEffect( () => {
+
+    useEffect(() => {
         fetch(`http://localhost:5000/myAdds/${user?.email}`)
-        .then(res => res.json())
-        .then(data =>{
-            console.log(data);
-            setSpots(data);
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setSpots(data);
+            })
     }, [])
+
+
+    const handelDelete = id => {
+        fetch(`http://localhost:5000/spots/${id}`, {
+            method: "DELETE"
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount){
+                const remaining = spots.filter(spot => spot._id !== id);
+                setSpots(remaining);
+                
+                alert('Deleted');
+            }
+        })
+
+    }
 
     return (
         <div>
             <h1 className="text-3xl">My Add</h1>
-            <div className="grid grid-cols-3 gap-6">
-                {
-                    spots.map(spot => <SpotCard key={spot._id} spot={spot}></SpotCard>)
-                }
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Spot Name</th>
+                            <th>Country</th>
+                            <th>Average Cost</th>
+                            <th>visitors Per Year</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            spots.map((spot, idx) => <tr key={idx}>
+                            <th>{idx + 1}</th>
+                            <td>{spot.spotName}</td>
+                            <td>{spot.counter}</td>
+                            <td>{spot.avgCost}</td>
+                            <td>{spot.visitorsPerYear}</td>
+                            <td><button><MdOutlineTipsAndUpdates className="text-xl text-rose-600 font-bold" /></button></td>
+                            <td><button onClick={() => handelDelete(spot._id)}><MdOutlineDeleteForever  className="text-xl text-rose-600 font-bold"/></button></td>
+                        </tr>)
+                        }
+                            {/* const {visitorsPerYear, travelTime, spotName, seasonality, photo, avgCost, _id} = spot; */}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
