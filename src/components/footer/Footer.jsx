deeb9714195f4c7a3916ed3bde38d748207db455
Tashdik <s2110477153@ru.ac.bd>
2024-04-27 17:53:39 +0600

    footer complete

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,10 @@ import foot2 from '../../assets/footerPic2-1.jpg'
 import foot3 from '../../assets/footerPic3-1.jpg'
 import foot4 from '../../assets/footerPic4-1.jpg'
 import foot5 from '../../assets/footerPic5-1.jpg'
-
+import './footer.css'
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdMarkEmailUnread } from "react-icons/md";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 
 
@@ -11,11 +14,11 @@ import { FaInstagramSquare, FaFacebookF, FaWhatsapp, FaTwitter } from "react-ico
 
 const Footer = () => {
     return (
-        <footer className="px-16 pt-16 pb-6 dark:bg-gray-100 dark:text-gray-800">
-            <div className="container grid grid-cols-2 mx-auto gap-x-5 gap-y-8 sm:grid-cols-3 md:grid-cols-6">
+        <footer className="relative pt-16  dark:bg-gray-100 dark:text-gray-800 footer-container">
+            <div className="container grid grid-cols-2 mx-auto gap-x-5 gap-y-8 sm:grid-cols-3 md:grid-cols-6 mb-16">
                 <div className="flex flex-col space-y-4 col-span-2 p-5">
-                    <p className="text-base font-semibold text-gray-600">TrekTrove: Your gateway to unforgettable adventures. Explore breathtaking landscapes, immerse in diverse cultures, and create lasting memories with us.</p>
-                    <h1 className="text-2xl font-semibold">FOLLOW ON US :</h1>
+                    <p className="text-base font-semibold text-white">TrekTrove: Your gateway to unforgettable adventures. Explore breathtaking landscapes, immerse in diverse cultures, and create lasting memories with us.</p>
+                    <h1 className="text-2xl font-semibold text-white">FOLLOW ON US :</h1>
                     <div className="space-x-3">
                         <button className="border p-4 rounded-full bg-rose-600"><FaInstagramSquare className="text-xl text-white" /></button>
                         <button className="border p-4 rounded-full bg-rose-600"><FaFacebookF className="text-xl text-white" /></button>
@@ -24,28 +27,28 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="flex flex-col space-y-4">
-                    <h2 className="text-2xl font-bold">Quick Link</h2>
+                    <h2 className="text-2xl font-bold text-white">Quick Link</h2>
                     <div className="flex flex-col space-y-2 text-sm dark:text-gray-600">
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">About US</a>
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">Tour Package</a>
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">Destination</a>
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">Tour Guide</a>
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">Booking Process</a>
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">Blog</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">About US</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">Tour Package</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">Destination</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">Tour Guide</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">Booking Process</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">Blog</a>
                     </div>
                 </div>
                 <div className="flex flex-col space-y-4">
-                    <h2 className="text-2xl font-bold">Tour Type</h2>
+                    <h2 className="text-2xl font-bold text-white">Tour Type</h2>
                     <div className="flex flex-col space-y-2 text-sm dark:text-gray-600">
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">Wild & Adventure Tours</a>
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">Group Tours</a>
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">Seasonal Tours</a>
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">Relaxation Tours</a>
-                        <a className="hover:text-rose-600 hover:font-bold text-lg" rel="noopener noreferrer" href="#">Family Friendly Tour</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">Wild & Adventure Tours</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">Group Tours</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">Seasonal Tours</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">Relaxation Tours</a>
+                        <a className="hover:text-rose-600 hover:font-bold text-lg text-white" rel="noopener noreferrer" href="#">Family Friendly Tour</a>
                     </div>
                 </div>
                 <div className="flex flex-col space-y-4 col-span-2">
-                    <h2 className="text-2xl font-bold text-center">Gallery</h2>
+                    <h2 className="text-2xl font-bold text-center text-white">Gallery</h2>
                     <div className="grid grid-cols-3 gap-2">
                         <div className='flex items-center justify-center'>
                             <img className='w-32 h-20 rounded-lg' src={foot1} alt="" />
@@ -68,8 +71,20 @@ const Footer = () => {
 
                 </div>
             </div>
-            <div className="flex items-center justify-center px-6 pt-12 text-sm">
+            {/* <div className="flex items-center justify-center px-6 pt-12 text-sm">
                 <span className="dark:text-gray-600">© Copyright 1986. All Rights Reserved.</span>
+            </div> */}
+            <div className='px-16'>
+                <hr />
+            </div>
+            <div className='grid grid-cols-8 mt-12 px-16'>
+                <h1 className='col-span-1 text-white flex items-center gap-2 text-2xl font-bold'>Contact Us :</h1>
+                <h1 className='col-span-2 text-white text-xl flex items-center justify-center gap-2'><FaPhoneAlt className='text-rose-600' /> +1593-648-96</h1>
+                <h1 className='col-span-2 text-white text-xl flex items-center justify-center gap-2'><MdMarkEmailUnread className='text-rose-600 text-2xl' /> info@trektrove.com</h1>
+                <h1 className='col-span-3 text-white text-xl flex items-center justify-center gap-2'><FaMapMarkerAlt className='text-rose-600 text-2xl' />  2752 Willison Street Eagan, United State</h1>
+            </div>
+            <div className='absolute bottom-0 w-full h-14 bg-zinc-800'>
+
             </div>
         </footer>
     );
